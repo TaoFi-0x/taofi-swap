27572f76d50013cbd17cdd358e5008f5667c46e1
Danilo <kitanovic.danilo@outlook.com>
2025-07-04 16:15:49 +0200

    fix: uniswap interface

--- contracts/interfaces/IUniswapV3Router.sol
+++ contracts/interfaces/IUniswapV3Router.sol
@@ -20,6 +20,7 @@ interface IUniswapV3Router {
         address tokenOut;
         uint24 fee;
         address recipient;
+        uint256 deadline;
         uint256 amountIn;
         uint256 amountOutMinimum;
         uint160 sqrtPriceLimitX96;
