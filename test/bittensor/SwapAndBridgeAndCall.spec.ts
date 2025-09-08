e028277dab1a00199f2d44dfed5e399c96bd9172
iris112 <nglayi99@gmail.com>
2025-09-08 22:03:26 +0900

    add test case of the Base Token -> TAO api

--- test/bittensor/SwapAndBridgeAndCall.spec.ts
+++ test/bittensor/SwapAndBridgeAndCall.spec.ts
@@ -12,6 +12,8 @@ const reverseCallURL = "https://taofi-api.web.app/getSellCall_Deprecated";
 const reverseCall2URL = "https://taofi-api.web.app/getSellCall";
 const reverseQuoteURL = "https://taofi-api.web.app/getSellQuote";
 const refundCallURL = "https://taofi-api.web.app/getRefundCall_Deprecated";
+const taoQuoteURL = "https://taofi-api.web.app/getTaoQuote";
+const taoURL = "https://taofi-api.web.app/getTaoCall";
 const refundCall2URL = "https://taofi-api.web.app/getRefundCall";
 const TransferCallURL = "https://taofi-api.web.app/getTransferCall";
 
@@ -77,8 +79,8 @@ const TransferCallURL = "https://taofi-api.web.app/getTransferCall";
 //         const fromTokenAddress = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";  //USDC (base)
 //         const fromDecimals = 6; // USDC decimals
 //         const fromAmount = "50000000"  // 50 USDC
-//         const subnetids = [4];
-//         // const subnetids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 96, 97, 98, 104, 105, 106, 107, 108, 111, 114, 123, 124];
+//         const subnetids = [120];
+//         // const subnetids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 96, 97, 98, 104, 105, 106, 107, 108, 111, 114, 120, 123, 124];
 //             // [103, 109]  unnecessary
         
 //         for (const subnetid of subnetids) {
@@ -116,7 +118,7 @@ const TransferCallURL = "https://taofi-api.web.app/getTransferCall";
 //             );
 //             console.log("Response data:", response.data);
 
-//             const fromToken = await ethers.getContractAt('ERC20', fromTokenAddress);
+//             const fromToken = await ethers.getContractAt('IERC20', fromTokenAddress);
 
 //             if ((await fromToken.allowance(deployer, response.data.to)).lt(fromAmount)) {
 //                 await fromToken.approve(response.data.to, "115792089237316195423570985008687907853269984665640564039457584007913129639935");
@@ -348,3 +350,53 @@ const TransferCallURL = "https://taofi-api.web.app/getTransferCall";
 //     } as SimpleTx)
 //   });
 // });
+
+describe('SwapAndBridgeAndCall', () => {
+    it('Swap, Bridge', async () => {
+        const { deployer } = await getNamedAccounts();
+        const { rawTx } = deployments;
+        const fromTokenAddress = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";  //USDC (base)
+        const fromDecimals = 6; // USDC decimals
+        const fromAmount = "1000000"  // 1 USDC
+        
+        let response = await axios.post(
+            taoQuoteURL,
+            {
+                fromTokenInfo: {
+                    address: fromTokenAddress,
+                    decimals: fromDecimals,
+                    amount: fromAmount
+                }
+            }
+        );
+        console.log("Response data:", response.data);
+
+        response = await axios.post(
+            taoURL,
+            {
+                sender: deployer,
+                fromTokenInfo: {
+                    address: fromTokenAddress,
+                    decimals: fromDecimals,
+                    amount: fromAmount
+                },
+                expectedTaoAmount: response.data.expectedTaoAmount,
+                slippage: 200,  // 2%
+            }
+        );
+        console.log("Response data:", response.data);
+
+        const fromToken = await ethers.getContractAt('IERC20', fromTokenAddress);
+
+        if ((await fromToken.allowance(deployer, response.data.to)).lt(fromAmount)) {
+            await fromToken.approve(response.data.to, "115792089237316195423570985008687907853269984665640564039457584007913129639935");
+        }
+
+        await rawTx({
+            value: response.data.hyperlaneFee,
+            to: response.data.to,
+            data: response.data.data,
+            from: deployer
+        } as SimpleTx)
+  });
+});
